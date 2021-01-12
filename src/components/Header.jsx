a79b7574aed76d6630f3e53d088caf437f398d73
Mika <mikanur10@gmail.com>
2021-01-11 22:36:57 -0500

    header fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,8 +6,8 @@ export default function Header() {
     <div id="header">
       <a href="#home">Home</a>
       <a href="#about">About</a>
-      <a href="#skills">Skills</a>
       <a href="#projects">Projects</a>
+      <a href="#skills">Skills</a>
       <a href="#contact">Contact</a>
       <a href="https://drive.google.com/file/d/1I1nHqA9cN0lsJgiMkWsuPKSJz6RKU51p/view?usp=sharing" target="_blank" rel="noopener noreferrer"> Resumé</a>
     </div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,8 +7,8 @@ export default function Nav(props) {
       <div id="navbar">
         <a href="#home">Home</a>
         <a href="#about">About</a>
-        <a href="#skills">Skills</a>
         <a href="#projects">Projects</a>
+        <a href="#skills">Skills</a>
         <a href="#contact">Contact</a>
         <a href="https://drive.google.com/file/d/1I1nHqA9cN0lsJgiMkWsuPKSJz6RKU51p/view?usp=sharing"> Resumé</a>
       </div>

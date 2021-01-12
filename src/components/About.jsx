429dcf2727620d182812544e190e78b19630c015
Mika <mikanur10@gmail.com>
2021-01-11 22:27:31 -0500

    about me update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,9 +5,9 @@ export default function About() {
   return (
     <div id="about">
       <h2 className="title"> About Me</h2>
-      <p className="paragraph">Junior software developer with 5-years of experience in enterprise technology sales. My experience with end-users and technologists have informed my ability to create a product that meets the needs of the user.
+      <p className="paragraph">Junior software developer with 5-years of experience in enterprise technology sales. My years of experience with end-users and technologists inform my ability to create a product that meets the needs of all stakeholders.
       I decided to transition into software engineering due to my experience in the technology industry and desire to build the very applications I was marketing.
-      My ability to empathize with people and communicate effectively equips me to anticipate customer needs and build great applications. I am able to bring these ideas to life using modern frameworks and efficient, scalable code.</p>
+      My approach to development is both empathetic and communicative - which enables me to anticipate customer needs and build great applications. I am able to bring these ideas to life using modern frameworks and efficient, scalable code.</p>
     </div>
   )
 }

9d17092bf8e3e6e5e6fad6a55363a9c83b382ea7
Mika <mikanur10@gmail.com>
2021-01-11 22:30:57 -0500

    change to about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ export default function About() {
       <h2 className="title"> About Me</h2>
       <p className="paragraph">Junior software developer with 5-years of experience in enterprise technology sales. My years of experience with end-users and technologists inform my ability to create a product that meets the needs of all stakeholders.
       I decided to transition into software engineering due to my experience in the technology industry and desire to build the very applications I was marketing.
-      My approach to development is both empathetic and communicative - which enables me to anticipate customer needs and build great applications. I am able to bring these ideas to life using modern frameworks and efficient, scalable code.</p>
+      My approach to development is both empathetic and communicative - which enables me to anticipate customer needs and build great applications. I am able to bring these ideas to life using modern frameworks and efficient, scalable code. I look forward to operating at the epicenter of development, product, marketing and design. </p>
     </div>
   )
 }
